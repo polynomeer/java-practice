f3c307fabc665bb07e92c67e2c2ffbe8c7bd10b4
Polynomeer <polynomeer@gmail.com>
2021-04-11 23:35:34 +0900

    Added OverrideTest.java to test @Override

---
+++ Basic/src/annotation/OverrideTest.java
@@ -0,0 +1,17 @@
+package annotation;
+
+public class OverrideTest {
+    private int id;
+    private String title;
+    private String content;
+
+    @Override
+    public String toString() {
+        return "OverrideTest{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
+

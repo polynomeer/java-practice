713de722d0a33d612afdadd9a190cf0bdb112dd2
Polynomeer <polynomeer@gmail.com>
2021-04-27 00:03:50 +0900

    Add StringToList to test logic

---
+++ Basic/src/string/StringToList.java
@@ -0,0 +1,23 @@
+package string;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StringToList {
+
+    public static List<String> toList(String s) {
+        s = s.replaceAll("\\[", "");
+        s = s.replaceAll("\\]", "");
+        s = s.replaceAll("\"", "");
+
+        String[] strings = s.split(",");
+        return Arrays.asList(strings);
+    }
+
+    public static void main(String[] args) {
+        String s = "[\"aaa\", \"bbb\", \"ccc\"]";
+        System.out.println(s);
+        List<String> list = toList(s);
+        System.out.println(list);
+    }
+}

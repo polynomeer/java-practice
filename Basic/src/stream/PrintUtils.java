655eab0b24853e25c42b3cd6afc52e9db6657599
polynomeer <polynomeer@gmail.com>
2022-02-05 23:30:20 +0900

    Change SIZE to the field of PrintUtils

--- Basic/src/stream/PrintUtils.java
+++ Basic/src/stream/PrintUtils.java
@@ -5,16 +5,18 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class PrintUtils {
+
+    private static final int SIZE = 10;
+
     /**
      * 스트림 요소를 출력하는 메서드
-     * 최대 10개 요소까지만 출력하도록 제한한다.
+     * 최대 SIZE 만큼의 요소를 출력하도록 제한한다.
      *
      * @param title  출력 제목
      * @param stream 출력 요소 스트림
      * @param <T>    출력 요소의 제네릭 타입
      */
     public static <T> void show(String title, Stream<T> stream) {
-        final int SIZE = 10;
         List<T> firstElements = stream.limit(SIZE + 1).collect(Collectors.toList());
         System.out.print(title + ": ");
         if (firstElements.size() <= SIZE)

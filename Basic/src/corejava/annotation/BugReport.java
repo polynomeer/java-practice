dea6859e798399d0af9503d7ff4c7e24f53498e2
polynomeer <polynomeer@gmail.com>
2022-02-17 23:29:23 +0900

    Update BugReport and Test to include description

--- Basic/src/corejava/annotation/BugReport.java
+++ Basic/src/corejava/annotation/BugReport.java
@@ -4,16 +4,28 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Repeatable;
 import java.lang.annotation.Target;
 
+/**
+ * 애너테이션은 요소라는 key/value 쌍을 포함할 수 있다.
+ * ⎷ 요소의 종류: 기본타입값, Class 객체, enum 인스턴스, 애너테이션, 이들의 배열
+ * ⎷ 애너테이션 요소는 절대로 null값을 가질 수 없다.
+ */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Repeatable(BugReports.class)
 public @interface BugReport {
     boolean showStopper() default false;
+
     String assignedTo() default "";
+
     Class<?> testCase() default Void.class;
+
     Status status() default Status.REPORTED;
+
     String[] reportedBy() default {};
-    Reference ref() default @Reference(id=0);
-    enum Status { REPORTED, CONFIRMED, ASSIGNED, FIXED, WONTFIX };
+
+    Reference ref() default @Reference(id = 0);
+
+    enum Status {REPORTED, CONFIRMED, ASSIGNED, FIXED, WONTFIX}
+
 }
 
 @Target({ElementType.TYPE, ElementType.METHOD})
--- Basic/src/corejava/annotation/Test.java
+++ Basic/src/corejava/annotation/Test.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * @Target 제한이 없는 애너테이션은 어떤 선언에든 쓸 수 있지만
+ * 타입 매개변수와 타입 사용에는 쓸 수 없다.
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Test {

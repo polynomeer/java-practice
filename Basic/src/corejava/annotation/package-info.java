78be3c7b2e7e4277c6d9d3d8215c6ae35f6451a7
polynomeer <polynomeer@gmail.com>
2022-02-17 09:57:57 +0900

    Add package-info.java

---
+++ Basic/src/corejava/annotation/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The Generated annotation is used to mark source code that has been generated.
+ * It can also be used to differentiate user written code from generated code in a single file.
+ */
+@Generated("com.august.generator")
+package corejava.annotation;
+
+import javax.annotation.processing.Generated;

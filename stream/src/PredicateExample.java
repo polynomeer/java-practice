38778cb0a18f09b423929143b1863a67925754c2
August <polynomeer@gmail.com>
2022-11-09 22:18:49 +0900

    refactor: Simplify with lambda expression

--- stream/src/PredicateExample.java
+++ stream/src/PredicateExample.java
@@ -5,20 +5,14 @@ import java.util.function.Predicate;
 
 public class PredicateExample {
     public static void main(String[] args) {
-        Predicate<Integer> isPositive = (Integer x) -> {
-            return x > 0;
-        };
+        Predicate<Integer> isPositive = x -> x > 0;
         System.out.println("isPositive = " + isPositive.test(-10));
 
         List<Integer> inputs = Arrays.asList(10, -5, 4, -2, 0, 3);
         System.out.println("Positive number: " + filter(inputs, isPositive));
         System.out.println("Non-positive number: " + filter(inputs, isPositive.negate()));
-        System.out.println("Non-negative number: " + filter(inputs, isPositive.or((Integer x) -> {
-            return x == 0;
-        })));
-        System.out.println("Positive even numbers: " + filter(inputs, isPositive.and((Integer x) -> {
-            return x % 2 == 0;
-        })));
+        System.out.println("Non-negative number: " + filter(inputs, isPositive.or(x -> x == 0)));
+        System.out.println("Positive even numbers: " + filter(inputs, isPositive.and(x -> x % 2 == 0)));
     }
 
     public static <T> List<T> filter(List<T> inputs, Predicate<T> condition) {

04ada8edf84f335a5555018dde9f8516d350e132
August <polynomeer@gmail.com>
2022-11-09 21:41:59 +0900

    refactor: Simplify with lambda expression

--- stream/src/ConsumerExample.java
+++ stream/src/ConsumerExample.java
@@ -4,20 +4,17 @@ import java.util.function.Consumer;
 
 public class ConsumerExample {
     public static void main(String[] args) {
-        Consumer<String> myStringConsumer = (String str) -> {
-            System.out.println("str = " + str);
-        };
+        Consumer<String> myStringConsumer = str ->
+                System.out.println("str = " + str);
         myStringConsumer.accept("hello");
 
         List<Integer> integerInputs = Arrays.asList(4, 2, 3);
-        Consumer<Integer> myIntegerProcessor = (Integer x) -> {
-            System.out.println("Processing Integer " + x);
-        };
+        Consumer<Integer> myIntegerProcessor = x ->
+                System.out.println("Processing Integer " + x);
         process(integerInputs, myIntegerProcessor);
 
-        Consumer<Integer> myDifferentIntegerProcessor = (Integer x) -> {
-            System.out.println("Processing Integer in different way " + x);
-        };
+        Consumer<Integer> myDifferentIntegerProcessor = x ->
+                System.out.println("Processing Integer in different way " + x);
         process(integerInputs, myDifferentIntegerProcessor);
     }
 

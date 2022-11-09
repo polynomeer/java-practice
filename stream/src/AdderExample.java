1a98699ab388bf12abbbea1e3bf3583ca02fd922
August <polynomeer@gmail.com>
2022-11-09 20:47:07 +0900

    feat: Add Adder example using lambda expression

--- stream/src/AdderExample.java
+++ stream/src/AdderExample.java
@@ -5,8 +5,12 @@ import java.util.function.Function;
 public class AdderExample {
 
     public static void main(String[] args) {
-        Function<Integer, Integer> adder = new Adder();
-        int result = adder.apply(5);
-        System.out.println("result = " + result);
+        Function<Integer, Integer> adder1 = new Adder();
+        int result1 = adder1.apply(5);
+        System.out.println("result1 = " + result1);
+
+        Function<Integer,Integer> adder2 = (Integer x) -> x + 10;
+        int result2 = adder2.apply(5);
+        System.out.println("result2 = " + result2);
     }
 }

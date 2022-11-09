01ad0a51df3eeff02b7697e2e31e9034cd628f77
August <polynomeer@gmail.com>
2022-11-09 22:07:01 +0900

    feat: Add BiConsumer in Functional Interface example

---
+++ stream/src/BiConsumerExample.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiConsumer;
+
+public class BiConsumerExample {
+    public static void main(String[] args) {
+        BiConsumer<Integer, Double> myDoubleProcessor = (Integer index, Double input)
+                -> System.out.println("Processing " + input + " at index = " + index);
+        List<Double> inputs = Arrays.asList(1.1, 2.2, 3.3);
+        process(inputs, myDoubleProcessor);
+    }
+
+    public static <T> void process(List<T> inputs, BiConsumer<Integer, T> processor) {
+        for (int i = 0; i < inputs.size(); i++) {
+            processor.accept(i, inputs.get(i));
+        }
+    }
+}

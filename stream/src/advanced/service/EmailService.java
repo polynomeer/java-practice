2d455bc6ebea78f03bd94d64384978f5750ca698
August <polynomeer@gmail.com>
2023-01-08 09:28:37 +0900

    feat: Add Stream example for Stream::forEach

--- stream/src/advanced/service/EmailService.java
+++ stream/src/advanced/service/EmailService.java
@@ -13,4 +13,9 @@ public class EmailService {
         user.getEmailAddress().ifPresent(email ->
                 System.out.println("Sending 'Make More Friends' email to " + email));
     }
+
+    public void sendVerifyYourEmailEmail(User user) {
+        user.getEmailAddress().ifPresent(email ->
+                System.out.println("Sending 'Verify Your Email' email to " + email));
+    }
 }

3dfa9021cea15dda332b3b1a94186a277b85c239
polynomeer <polynomeer@gmail.com>
2025-07-31 23:38:49 +0900

    feat: implement quick sort for SortStrategy

--- patterns/src/main/java/com/polynomeer/strategy/QuickSort.java
+++ patterns/src/main/java/com/polynomeer/strategy/QuickSort.java
@@ -1,10 +1,42 @@
 package com.polynomeer.strategy;
 
 public class QuickSort implements SortStrategy {
+
     @Override
     public void sort(int[] arr) {
         System.out.println("Quick Sort 수행");
-        java.util.Arrays.sort(arr);
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
     }
 }
+
 
